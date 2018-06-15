84b2fa16cb8991ca3342da7d1aa24016c3380d99
Christian Holz <christian@christianholz.net>
2018-06-14 23:04:07 -0700

    sort summary by leader

--- list.py
+++ list.py
@@ -174,7 +174,7 @@ print '''
 <h1>Summary</h1>
 <table id="summary">
   <tr><th class="summary_left"></th>''' + ''.join(['<th>%s</th>' % s for s in config['done']]) + '''<th>touched</th><th>total</th></tr>'''
-for un in sorted(auth_stat.keys()):
+for un in sorted(auth_stat.keys(), key=lambda x:sum(auth_stat[x]) - auth_stat[x][1], reverse=True):
     print '  <tr><td class="summary_left">%s</td>' % un + ''.join(['<td>%d</td>' % i for i in auth_stat[un]]) + '<td>%d</td><td>%d</td></tr>' % (sum(auth_stat[un]) - auth_stat[un][1], sum(auth_stat[un]))
 print '''</table>
 </body>
